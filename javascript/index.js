355aeb9dfa9ddbbc62944773d4baa81038bd495f
Kyun2Da <dktnskdktnsk@gmail.com>
2020-01-20 00:14:01 +0900

    [add] 홈페이지 만들어보기

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+document.write("<h1>로그인</h1>");
+document.write("<input type='text' name = '아이디' placeholder='아이디'> ");
+document.write("<input type='text' name = '비밀번호' placeholder='비밀번호'>");
+document.write("<button type='button'>로그인</button>");
