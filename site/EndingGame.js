2283a4720ec2970194fc4f5173b63911025b2a03
Kyun2da <dktnskdktnsk@gmail.com>
2020-02-17 23:41:23 +0900

    [Update] 끝말잇기 디자인 변경

--- site/EndingGame.js
+++ site/EndingGame.js
@@ -22,6 +22,11 @@ var 플레이어1;
 var 플레이어2;
 var 플레이어1공간 = document.getElementById("플레이어1공간");
 var 플레이어2공간 = document.getElementById("플레이어2공간");
+var 플레이어1목숨;
+var 플레이어2목숨;
+var 시작목숨 = 3;
+var 목숨1 = document.getElementById("목숨1");
+var 목숨2 = document.getElementById("목숨2");
 시작버튼.addEventListener("click", function(e) {
   $("#myModal").modal();
 });
@@ -35,6 +40,10 @@ playButton.addEventListener("click", function(e) {
   플레이어.textContent = 플레이어1 + "님의 차례입니다.";
   플레이어1이름.textContent = "플레이어1 : " + 플레이어1;
   플레이어2이름.textContent = "플레이어2 : " + 플레이어2;
+  플레이어1목숨 = 시작목숨;
+  플레이어2목숨 = 시작목숨;
+  목숨1만들기(시작목숨);
+  목숨2만들기(시작목숨);
   입력창.focus();
 });
 
@@ -50,9 +59,12 @@ playButton.addEventListener("click", function(e) {
     입력창2.focus();
   } else {
     결과창.textContent = "땡!";
+    플레이어1목숨--;
     입력창.value = "";
     입력창2.focus();
   }
+  목숨1만들기(플레이어1목숨);
+  목숨2만들기(플레이어2목숨);
 });
 
 버튼2.addEventListener("click", function 콜백함수() {
@@ -67,14 +79,35 @@ playButton.addEventListener("click", function(e) {
     입력창.focus();
   } else {
     결과창.textContent = "땡!";
+    플레이어2목숨--;
     입력창2.value = "";
     입력창.focus();
   }
+  목숨1만들기(플레이어1목숨);
+  목숨2만들기(플레이어2목숨);
 });
 function enterkey() {
   if (window.event.keyCode === 13) {
     // 엔터키가 눌렸을 때 실행할 내용
     if (turn % 2 == 1) 버튼.click();
     else 버튼2.click();
+  }
+}
+
+function 목숨1만들기(num) {
+  목숨1.innerHTML = "";
+  for (var i = 0; i < num; i++) {
+    var div = document.createElement("div");
+    div.classList.add("목숨");
+    목숨1.appendChild(div);
+  }
+}
+
+function 목숨2만들기(num) {
+  목숨2.innerHTML = "";
+  for (var i = 0; i < num; i++) {
+    var div = document.createElement("img");
+    div.classList.add("목숨");
+    목숨2.appendChild(div);
   }
 }
